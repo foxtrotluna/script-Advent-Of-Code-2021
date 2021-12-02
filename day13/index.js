f301691173c1d3078a552b7e971337d4dec76f20
Luna Winters <luna@gocreate.fun>
2021-12-02 11:18:01 +0000

    Log which part is running

--- day13/index.js
+++ day13/index.js
@@ -6,7 +6,9 @@ import Part2 from './parts/part2.js'
 const filename = process.env.TEST ? './inputs/testinput.txt' : './inputs/input.txt'
 const run = async () =>{
     const buffer = await readFileAsync(filename)
+    console.log('Part 1:')
     Part1(buffer)
+    console.log('------\n Part 2:')
     Part2(buffer)
 }
 
