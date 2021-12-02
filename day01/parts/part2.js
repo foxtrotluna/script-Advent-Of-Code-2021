eac9271b2f45c745e6e41c1248588da9be052f61
Luna Winters <luna@gocreate.fun>
2021-12-02 11:38:56 +0000

    Added Part 2

--- day01/parts/part2.js
+++ day01/parts/part2.js
@@ -1,6 +1,16 @@
 const Part1 = async (buffer) =>{
     const input = buffer.toString()
     const split = input.split('\n')
+    let count = 0 // Times the depth measurement increases
+    let previous = parseInt(split[0])+parseInt(split[1])+parseInt(split[2]) // The previous depth measurement
+    for (let i=0;i<split.length-2;i++){
+        const sum = parseInt(split[i])+parseInt(split[i+1])+parseInt(split[i+2])
+        if (sum > previous) {
+            count++
+        }
+        previous = sum
+    }
+    console.log(count)
 }
 
 export default Part1
