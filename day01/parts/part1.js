5f8e6d3a283a392b677684586ff2ced51076116b
Luna Winters <luna@gocreate.fun>
2021-12-02 11:18:11 +0000

    Day 1 Part 1 solution

--- day01/parts/part1.js
+++ day01/parts/part1.js
@@ -1,7 +1,15 @@
 const Part1 = async (buffer) =>{
     const input = buffer.toString()
     const split = input.split('\n')
-    console.log(split)
+    let count = 0 // Times the depth measurement increases
+    let previous = split[0] // The previous depth measurement
+    for (const depth of split){
+        if (parseInt(depth) > parseInt(previous)) {
+            count++
+        }
+        previous = depth
+    }
+    console.log(count)
 }
 
 export default Part1
