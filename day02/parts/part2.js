5b21f1b51f71791074deeb1ae488d568870e4c78
Luna Winters <luna@gocreate.fun>
2021-12-02 11:53:28 +0000

    Added solution for Day 2 Part 2

--- day02/parts/part2.js
+++ day02/parts/part2.js
@@ -1,6 +1,25 @@
 const Part1 = async (buffer) =>{
     const input = buffer.toString()
     const split = input.split('\n')
+    let horizontal = 0
+    let vertical = 0
+    let aim = 0
+    for(const command of split){
+        const values = command.split(' ')
+        if (values[0] == 'forward'){
+            horizontal += parseInt(values[1])
+            vertical += (aim*parseInt(values[1]))
+        }
+        if (values[0] == 'down'){
+            aim += parseInt(values[1])
+        }
+        if (values[0] == 'up'){
+            aim -= parseInt(values[1])
+        }
+    }
+    console.log('H: '+horizontal)
+    console.log('V: '+vertical)
+    console.log('Product: '+(horizontal*vertical))
 }
 
 export default Part1
